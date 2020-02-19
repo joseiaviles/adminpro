9790208fb75e150c862093860eddc89ad2826fa1
Jose Ignacio Aviles <Joseignacio_a@yahoo.com>
2020-02-19 14:46:29 -0500

    Input, Output, ViewChild

---
+++ angular adv/adminpro/src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MultiDataSet, Label } from 'ng2-charts';
+// import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styles: []
+})
+export class GraficoDonaComponent implements OnInit {
+
+  @Input('chartLabels') doughnutChartLabels: Label[] = [];
+  @Input('chartData') doughnutChartData: MultiDataSet = [];
+  @Input('chartType') doughnutChartType: string = '';
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular adv/adminpro/src/app/pages/pages.modulo.ts
+++ angular adv/adminpro/src/app/pages/pages.modulo.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -8,13 +11,20 @@ import { PagesComponent } from './pages.component';
 // Modulos
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
+//  graficas
+import { ChartsModule } from 'ng2-charts';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     PagesComponent,
@@ -24,7 +34,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 })
 export class PagesModule {}
